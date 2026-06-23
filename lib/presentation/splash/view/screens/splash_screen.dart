54e555c5332776be947968337245e5b158c6bb78
moazosama1 <www.moazosama525@gmail.com>
2026-06-23 05:40:41 +0300

    fix: navigation

--- lib/presentation/splash/view/screens/splash_screen.dart
+++ lib/presentation/splash/view/screens/splash_screen.dart
@@ -19,7 +19,7 @@ class SplashScreen extends StatelessWidget {
         if (state is SplashNavigateToOnboarding) {
           context.go(RouteNames.onboarding);
         } else if (state is SplashNavigateToLogin) {
-          context.go(RouteNames.aiChat);
+          context.go(RouteNames.login);
         } else if (state is SplashNavigateToHome) {
           context.go(RouteNames.home);
         } else if (state is SplashNavigateToHrHome) {
